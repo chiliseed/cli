71d528c7872e9f10963fc2d206ca7f8bc0a53704
Altonotch <info@altonotch.com>
2020-02-05 23:08:05 +0200

    Central api client setup

--- src/main.rs
+++ src/main.rs
@@ -15,19 +15,19 @@ fn main() {
         .expect("Cannot initialize the logger that was already initialized.");
 
     info!("Firing up chiliseed CLI");
+    let api_client = match APIClient::new() {
+        Ok(c) => c,
+        Err(err) => {
+            error!("{}", err);
+            exit(1)
+        }
+    };
     let args = Opt::from_args();
 
     match args.cmd {
         Command::Environment { cmd } => match cmd {
             EnvSubCommands::List {} => {
                 info!("Getting a list of environments");
-                let api_client = match APIClient::new() {
-                    Ok(c) => c,
-                    Err(err) => {
-                        error!("{}", err);
-                        exit(1)
-                    }
-                };
                 match api_client.list_envs() {
                     Ok(envs) => {
                         info!("{:?}", envs);
