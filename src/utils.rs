3696649c63aab56225f02badb963e6ca6526bae3
Altonotch <info@altonotch.com>
2020-04-30 22:48:35 +0300

    Add utilities for table output
    Signed-off-by: Altonotch <info@altonotch.com>

--- src/utils.rs
+++ src/utils.rs
@@ -3,11 +3,17 @@ use std::thread::sleep;
 use std::time::Duration;
 
 use crate::api_client::ApiClient;
+use prettytable::{format, Cell, Row, Table};
 
 const WAIT_TIME_SECS: u64 = 10;
+const WAIT_SERVER_TIMEOUT_MINUTES: u64 = 30;
 
-pub fn await_exec_result(api_client: &ApiClient, run_slug: &str) -> bool {
-    let timeout_minutes = 30;
+pub fn await_exec_result(
+    api_client: &ApiClient,
+    run_slug: &str,
+    timeout_minutes: Option<u64>,
+) -> bool {
+    let timeout_minutes = timeout_minutes.unwrap_or(WAIT_SERVER_TIMEOUT_MINUTES);
     let mut waited = 0;
     loop {
         if waited >= timeout_minutes * 60 {
@@ -58,3 +64,17 @@ pub fn exec_command_with_output(
     Ok((output.status.success(), output.stdout))
 }
 
+pub fn get_output_table() -> Table {
+    let mut table = Table::new();
+    let format = format::FormatBuilder::new().column_separator('\t').build();
+    table.set_format(format);
+    table
+}
+
+pub fn add_row_to_output_table(table: &mut Table, values: Vec<&str>) {
+    let mut cells: Vec<Cell> = Vec::new();
+    for v in values {
+        cells.push(Cell::new(v));
+    }
+    table.add_row(Row::new(cells));
+}
