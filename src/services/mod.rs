a85749fdfbbc29dcaeafa2db1f0be16e24ddf160
Altonotch <info@altonotch.com>
2020-05-14 14:51:05 +0300

    Rename add bucket to statics bucket
    - add remove statics bucket
    
    Signed-off-by: Altonotch <info@altonotch.com>

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,16 +1,16 @@
-mod add_bucket;
 mod add_db;
 mod create;
 mod deploy;
 mod list;
+mod statics_bucket;
 mod types;
 mod update;
 mod utils;
 
-pub use self::add_bucket::*;
 pub use self::add_db::*;
 pub use self::create::*;
 pub use self::deploy::*;
 pub use self::list::*;
+pub use self::statics_bucket::*;
 pub use self::update::*;
 pub use self::utils::*;
--- src/services/statics_bucket.rs
+++ src/services/statics_bucket.rs
@@ -3,6 +3,8 @@ use crate::env_vars;
 use crate::schemas::{Project, Service};
 use crate::utils::await_exec_result;
 
+const STATICS_BUCKET_KEY_NAME: &str = "AWS_STATICS_STORAGE_BUCKET_NAME";
+
 pub fn add_statics(api_client: &ApiClient, service: Service, project: Project) {
     let (run_slug, bucket_slug) = match api_client.create_statics_bucket(
         &service.slug,
@@ -30,12 +32,42 @@ pub fn add_statics(api_client: &ApiClient, service: Service, project: Project) {
             }
         };
         debug!("new resource: {:?}", bucket);
-        let bucket_name_key = "AWS_STATICS_STORAGE_BUCKET_NAME";
-        env_vars::create(api_client, service, bucket_name_key, &bucket.identifier);
+        env_vars::create(
+            api_client,
+            service,
+            STATICS_BUCKET_KEY_NAME,
+            &bucket.identifier,
+        );
         println!(
             "Bucket name will be injected into your containers under following key: {}",
-            bucket_name_key
+            STATICS_BUCKET_KEY_NAME
         );
         println!("Redeploy {} services to see this variable.", project.name);
+    }
+}
+
+pub fn remove_statics(api_client: &ApiClient, service: Service) {
+    let run_log = match api_client.remove_statics_bucket(&service.slug) {
+        Ok(resp) => resp.log,
+        Err(err) => {
+            debug!("Server error: {}", err);
+            println!("Server error. Please try again later.");
+            return;
+        }
+    };
+
+    println!("Removing statics infra");
+    let is_success = await_exec_result(api_client, &run_log, None);
+
+    if is_success {
+        println!("Removing statics env var");
+        if env_vars::delete_env_var(api_client, &service.slug, STATICS_BUCKET_KEY_NAME) {
+            println!("Statics bucket env var removed successfully");
+        } else {
+            eprintln!(
+                "There was an error removing env var {}. Did it exist",
+                STATICS_BUCKET_KEY_NAME
+            )
+        }
     }
 }
